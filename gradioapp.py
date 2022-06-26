d6016454bb5e1fe8eeb80867f6526dc09236b629
Rishi R <56316487+Rishi0812@users.noreply.github.com>
2022-06-26 13:13:01 +0530

    Delete gradioapp.py

--- gradioapp.py
+++
@@ -1,7 +0,0 @@
-from main import *
-import gradio as gr
-
-gr.Interface(fn=chatbot_response,
-            title = "KIRA Bot",
-            inputs=["text"],
-            outputs=["chatbot", "audio"]).launch(debug=True)
